1c513715c7de265e5957158545b8b442c572bf65
MukamaJ-2 <josephmukama67@gmail.com>
2024-09-28 09:03:06 +0300

    Update recursive.py

--- recursive.py
+++ recursive.py
@@ -1,9 +1,16 @@
-def fn_h(k):
-    if (k > 0):
-         result = k + fn_h(k - 1)
-         print(result)
-    else:
-     result = 0
-    return result
-print("recursed numbers:")
-fn_h(9)
+class Person:
+    def __init__(self, full_names, age, work_place, level_of_education, university_you_went_to):
+        self.your_names = full_names
+        self.your_age = age
+        self.your_work_place = work_place
+        self.your_level_of_education = level_of_education
+        self.your_university = university_you_went_to
+    def printname(self):
+        print(self.your_names, self.your_age, self.your_work_place, self.your_level_of_education, self.your_university)
+
+p = Person("My name is musinguzi Marcellinas",
+            "aged 23",
+            "working as a software developer in Silicon Valley",
+            "and i persued a Bachelors of Science in Computer Science",
+             "Uganda Christian University")
+p.printname()
